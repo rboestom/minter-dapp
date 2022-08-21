394e16adde146be7291aced2eeeab59be3096e69
rizky aditya boestom <44334572+rboestom@users.noreply.github.com>
2022-08-21 07:18:05 +0700

    update

--- backend/src/config.js
+++ backend/src/config.js
@@ -60,7 +60,7 @@ const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Ethereum = ET
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-09-23T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-08-21T00:19:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-08-21T00:17:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
